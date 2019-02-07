61e2f54eade725c33f2494f0a55a217fee1657e5
vvegorov <egorovvvsls@gmail.com>
2019-02-07 16:01:36 +0300

    7
    7 февраля

--- sweater/src/main/java/com/example/sweater/config/MvcConfig.java
+++ sweater/src/main/java/com/example/sweater/config/MvcConfig.java
@@ -2,7 +2,9 @@ package com.example.sweater.config;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,6 +14,12 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Value("upload.path")
     private String uploadPath;
+
+    @Bean
+    RestTemplate getRestTemplate(){
+
+        return new RestTemplate();
+    }
 
 
     public void addViewControllers(ViewControllerRegistry registry) {
--- sweater/src/main/java/com/example/sweater/controler21/RegistrationController.java
+++ sweater/src/main/java/com/example/sweater/controler21/RegistrationController.java
@@ -1,8 +1,10 @@
 package com.example.sweater.controler21;
 
 import com.example.sweater.domain.User;
+import com.example.sweater.domain.dto.CaptchaResponseDto;
 import com.example.sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -11,14 +13,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
+import java.util.Collections;
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
+
+    private final static String CAPTCHA_URL = "https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s";
+
+
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${recaptcha.secret}")
+    private String secret;
 
     @GetMapping("/registration")
     public String registration(){
@@ -28,9 +42,18 @@ public class RegistrationController {
     @PostMapping("/registration")
     public String addUser(
             @RequestParam("password2") String passwordConfirm,
+            @RequestParam("g-recaptcha-response") String captchaResponce,
             @Valid User user,
             BindingResult bindingResult,
             Model model){
+        String url = String.format(CAPTCHA_URL, secret, captchaResponce);
+        CaptchaResponseDto response = restTemplate.postForObject(url, Collections.emptyList(), CaptchaResponseDto.class);
+
+        if(!response.isSuccess()){
+            model.addAttribute("captchaError","Fill captcha");
+        }
+
+
         boolean isConfirmEmpty = StringUtils.isEmpty(passwordConfirm);
 
         if(isConfirmEmpty){
@@ -43,7 +66,7 @@ public class RegistrationController {
             model.addAttribute("passwordError", "Passwords are different!");
         }
 
-        if(isConfirmEmpty || bindingResult.hasErrors()){
+        if(isConfirmEmpty || bindingResult.hasErrors() || !response.isSuccess()){
             Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
 
             model.mergeAttributes(errors);
